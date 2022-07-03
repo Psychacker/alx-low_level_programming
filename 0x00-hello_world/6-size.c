1d9c11ed79dd943db3f2959c990c8a2eb2ae3cc9
psychacker <abidemiahmed123@gmail.com>
2022-07-03 15:25:16 -0700

    C language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
 	printf("Size of a float: %lu byte(s)\n", sizeof(d));
-	return (0) ;
+	return (0);
 }
