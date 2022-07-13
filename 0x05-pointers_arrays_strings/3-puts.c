7baebad940116d5953b5dad39dbd76034c2f5de4
psychacker <abidemiahmed123@gmail.com>
2022-07-13 15:09:53 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,11 +10,11 @@
 
 void _puts(char *str)
 {
-	int index;
+int index;
 
-	for (index = 0; str[index] != '0\'; index++
-		{
-			_putchar(str[index]);
-		}
-			_putchar('\n');
-			}
+for (index = 0; str[index] != '\0'; index++)
+{
+_putchar(str[index]);
+}
+_putchar('\n');
+}
