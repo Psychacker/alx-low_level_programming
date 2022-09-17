963d8848eabbf6a4b7be18f2abd144cc2f7742f7
psychacker <abidemiahmed123@gmail.com>
2022-09-17 04:51:20 -0700

    nothung

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: input.
+ *
+ * Return: 1 if c is a digit
+ * 0 if anything else
+ */
+
+int _isdigit(int c)
+{
+if (c >= 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
