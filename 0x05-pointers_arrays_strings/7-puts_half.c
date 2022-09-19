26f3046d9310bb8db5f87bdb4c52329317e15a0b
psychacker <abidemiahmed123@gmail.com>
2022-09-19 07:11:44 -0700

    Pointers Array Strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,17 @@
  */
 void puts_half(char *str)
 {
-		int index, half;
-
-			index = 0;
-				while (str[index] != '\0')
-							index++;
-
-					half = index / 2;
-
-						if (index % 2 == 1)
-									half++;
-
-							while (half < index)
-									{
-												_putchar(str[half]);
-														half++;
-															}
-								_putchar('\n');
+int index, half;
+index = 0;
+while (str[index] != '\0')
+index++;
+half = index / 2;
+if (index % 2 == 1)
+half++;
+while (half < index)
+{
+_putchar(str[half]);
+half++;
+}
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-		char *start = dest;
-
-			while (*src != '\0')
-					{
-								*dest = *src;
-										dest++;
-												src++;
-													}
-				*dest = '\0';
-					return (start);
+char *start = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
 }
